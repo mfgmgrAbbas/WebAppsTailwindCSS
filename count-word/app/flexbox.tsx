058482479698589e40ec5bc8b6581ded30736dcf
smasnb12 <smasnb12@gmail.com>
2023-03-19 10:09:22 +0500

    update

---
+++ count-word/app/flexbox.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function flexbox() {
+  return (
+    <div>
+        <div>Home</div>
+        <div>About</div>
+        <div>Contact</div>
+    </div>
+  )
+}
